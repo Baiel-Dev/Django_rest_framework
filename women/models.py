6a3e37688eb7357bafdc42b0d1a06bc13d81f38b
Baiel-Dev <print.baiel.@gmail.com>
2025-01-24 23:14:18 +0600

    lesonn 2

--- women/models.py
+++ women/models.py
@@ -13,7 +13,7 @@ class Category(models.Model):
 
 class Women(models.Model):
     title = models.CharField(max_length=100)
-    contend = models.TextField(blank=True)
+    content = models.TextField(blank=True)
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)

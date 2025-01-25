091d80dcd46863c4039746cd175028a9fd0a299f
Baiel-Dev <print.baiel.@gmail.com>
2025-01-26 01:01:43 +0600

    lseonn 3

--- women/views.py
+++ women/views.py
@@ -1,4 +1,5 @@
 from django.forms import model_to_dict
+from django.utils.autoreload import raise_last_exception
 from rest_framework import generics
 from django.shortcuts import render
 from rest_framework.response import Response
@@ -16,16 +17,32 @@ class WomenAPIView(APIView):
     def post(self, request):
         serializer = WomenSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+        serializer.save()
 
-        post_new = Women.objects.create(
-            title=request.data['title'],
-            content=request.data['content'],
-            cat_id=request.data['cat_id']
-        )
-
-        return Response({'post': WomenSerializer(post_new).data})
+        return Response({'post': serializer.data})
 
 
-# class WomenAPIView(generics.ListAPIView):
-#     queryset = Women.objects.all()
-#     serializer_class = WomenSerializer
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+                return Response({"error":"Method PUT not allowed"})
+
+        try:
+            instance = Women.objects.get(pk=pk)
+        except:
+            return Response({"error": "Objects dose not exits"})
+
+        serializer = WomenSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception = True)
+        serializer.save()
+        return Response({"post": serializer.data})
+
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get("pk", None)
+        if not pk:
+            return Response({"error": "Method Delete not allowed"})
+
+
+
+        return Response({"post": "delete post" +str(pk)})
